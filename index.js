ea1cbc9cd3a46a6d9ba9683f368b27b502480f72
gcdxuzhiwei <gcdxuzhiwei@gmail.com>
2021-04-15 20:19:03 +0800

    no message

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 var MongoClient = require("mongodb").MongoClient;
 
+const uuid = () => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    // eslint-disable-next-line no-bitwise
+    const r = (Math.random() * 16) | 0;
+    // eslint-disable-next-line no-bitwise
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
+
 MongoClient.connect(
   "mongodb://47.103.217.181:27017",
   { useUnifiedTopology: true },
@@ -10,32 +20,59 @@ MongoClient.connect(
     chat = client.db("chat").collection("chat");
 
     io.on("connection", function (socket) {
-      socket.on("room", async (room) => {
-        socket.join(room);
+      socket.on("userId", async (userId) => {
         const hasRoom = await chat.findOne({
-          room,
+          [userId[0]]: 1,
+          [userId[1]]: 1,
         });
+        let roomId = "";
+        let detail = [];
         if (!hasRoom) {
+          roomId = uuid();
           await chat.insertOne({
-            room,
+            [userId[0]]: 1,
+            [userId[1]]: 1,
+            room: roomId,
             detail: [],
           });
         } else {
-          socket.emit("chat", hasRoom.detail);
+          roomId = hasRoom.room;
+          detail = hasRoom.detail;
         }
-        socket.on("add", async (e) => {
-          const findRoom = await chat.findOne({
-            room: e.room,
-          });
-          await chat.update(
-            { room: e.room },
-            {
-              room: e.room,
-              detail: [e, ...findRoom.detail],
-            }
-          );
-          io.to(e.room).emit("addChat", e);
+        socket.join(roomId);
+        io.to(roomId).emit("allList", {
+          detail,
+          roomId: roomId,
         });
+      });
+      socket.on("addItem", async (item) => {
+        const old = await chat.findOne({
+          room: item.roomId,
+        });
+        const newItem = {
+          info: item.info,
+          userId: item.userId,
+          time: new Date().getTime(),
+        };
+        await chat.update(
+          { room: item.roomId },
+          {
+            ...old,
+            room: item.roomId,
+            detail: [...old.detail, newItem],
+          }
+        );
+        io.to(item.roomId).emit("addItem", newItem);
+      });
+
+      socket.on("getList", async (item) => {
+        const list = await chat
+          .find({
+            [item[0]]: 1,
+          })
+          .toArray();
+        socket.join(item[0]);
+        io.to(item[0]).emit("postList", list);
       });
     });
 
